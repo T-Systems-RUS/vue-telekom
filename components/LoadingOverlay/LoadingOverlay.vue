79b07fc43a22c6a01accfc94dda53b1af306976a
mzymta <ekonyakhin@gmail.com>
2018-11-16 16:13:09 +0300

    refactored LoadingOverlay component

--- components/LoadingOverlay/LoadingOverlay.vue
+++ components/LoadingOverlay/LoadingOverlay.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="loading-overlay">
     <div
-      :class="{'progress-bar-animation': isLoading}"
-      class="header-bar-top"/>
-    <div
-      v-if="isLoading"
-      class="loading-state-overlay"/>
+      :class="{'is-loading': isLoading}"
+      class="loading-overlay-bar"/>
+    <transition name="opacity">
+      <div
+        v-if="isLoading"
+        class="loading-overlay-cover"/>
+    </transition>
   </div>
 </template>
 
@@ -27,38 +29,64 @@
   @import '../../styles/bulma/bulma-override-variables';
   @import '../../styles/utilities/mixins';
 
+  $loading-bar-height: 4px;
+  $animation-time: 2.5s;
+  $long-animation-time: 10s;
+  $animation-delay: $animation-time;
 
-  .progress-bar-animation {
-    animation-name: move-progress-bar;
-    animation-duration: 2.5s;
-    animation-fill-mode: forwards;
+  .loading-overlay {
+    &-bar {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: $loading-bar-height;
+      background-color: $magenta;
+      z-index: 10;
+
+      &.is-loading {
+        animation: loading-bar $animation-time forwards,
+                   loading-bar-long $long-animation-time $animation-delay linear infinite;
+      }
+    }
+
+    &-cover {
+      position: fixed;
+      height: 100%;
+      width: 100%;
+      top: $loading-bar-height;
+      left: 0;
+      background-color: $white;
+      opacity: 0.6;
+      z-index: 1000;
+    }
+
+    &.is-absolute {
+      .loading-overlay-bar,
+      .loading-overlay-cover {
+        position: absolute;
+      }
+    }
   }
 
-  .loading-state-overlay {
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    background-color: $white;
-    opacity: 0.6;
-    z-index: 1000;
-    top: 4px;
-  }
-
-  .header-bar-top {
-    width: 100%;
-    height: 4px;
-    background-color: $magenta;
-    position: fixed;
-    top: 0;
-    z-index: 10;
-  }
-
-  @keyframes move-progress-bar {
+  @keyframes loading-bar {
     from {
-      width: 10%;
+      width: 0%;
     }
     to {
-      width: 90%;
+      width: 95%;
+    }
+  }
+
+  @keyframes loading-bar-long {
+    0% {
+      width: 95%;
+    }
+    90% {
+      width: 98%;
+    }
+    100% {
+      width: 95%;
     }
   }
 </style>
